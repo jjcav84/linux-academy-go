95a34e6f768b51a32ee8cd7bf623c28c05af597a
Jacob Cavazos <jjcav84@gmail.com>
2019-03-08 07:29:52 +0000

    added numbers initial program

---
+++ common_data_types/learn_numbers.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Addition:", 1.0+3)
+	fmt.Println("Subtraction:", 27-19.5)
+	fmt.Println("Multiplication:", 9.801*11)
+	fmt.Println("Division:", 20/3.0)
+	fmt.Println("Exponents", math.Pow(7, 3))
+}
