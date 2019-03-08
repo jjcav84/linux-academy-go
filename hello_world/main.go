1613cda8a638c903a79fffb41b102e5bef030537
Jacob Cavazos <jjcav84@gmail.com>
2019-03-08 06:17:48 +0000

    added single and multi line comments to the hello world program

--- hello_world/main.go
+++ hello_world/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
-func main () {
-	fmt.Println("Hello Linux Academy")	
+/*
+this is a comment
+on multiple lines
+*/
+
+//main is the primary function for our package.
+//This function prints a message to the screen
+func main() {
+	//this is a single line comment
+	fmt.Println("Hello Linux Academy")
 }
