86c4c4e3ce98e6f07fa15a9e15e6cde780bfa880
Jacob Cavazos <jjcav84@gmail.com>
2019-03-08 07:20:09 +0000

    strings program

---
+++ common_data_types/learn_string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Simple String")
+	fmt.Println(`
+This is a multi-line
+String, that can also contain "quotes".
+`)
+	fmt.Println("🙂")
+	fmt.Println("\u2272")
+}
