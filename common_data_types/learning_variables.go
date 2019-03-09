8a39303cb2ec08b3de7acf690cd69c00fed53b2b
Jacob Cavazos <jjcav84@gmail.com>
2019-03-09 06:16:42 +0000

    added first variable program to the repository

---
+++ common_data_types/learning_variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myInt int = 16
+	var val, ok = "yes", true
+
+	fmt.Println("myInt is:", myInt)
+	fmt.Println("myInt times two:", myInt*2)
+	fmt.Println("val is:", val)
+	fmt.Println("ok is:", ok)
+}
