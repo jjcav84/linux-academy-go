8f21bc9c597e4458a7ffa417e34e46b768ab8a96
Jacob Cavazos <jjcav84@gmail.com>
2019-03-09 06:21:52 +0000

    added first variable program with shorthand notation to the repository

---
+++ common_data_types/learning_variables_2.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var val, _ = "yes", true
+
+	myInt := 16
+
+	name = "Keith"
+
+	fmt.Println("myInt is:", myInt)
+	fmt.Println("myInt times two:", myInt*2)
+	fmt.Println("val is:", val)
+	fmt.Println("name is:", name)
+}
